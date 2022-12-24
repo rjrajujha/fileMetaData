d3c1c892af1abc6ca416cdc91b6be8b0fffb1c32
Raju Jha <63865197+rjrajujha@users.noreply.github.com>
2022-12-24 08:15:48 +0000

    Project Completed on replit

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' })
+// const bodyParser = require('body-parser');
+
 
 var app = express();
+
+// parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  // req.body contains the text fields
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
+
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
 });
